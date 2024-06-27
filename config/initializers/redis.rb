b74226233559810645bac764ed5ced6f6e78d2fd
Ubuntu <ubuntu@ip-172-31-33-93.ap-south-1.compute.internal>
2024-06-27 11:34:44 +0000

    redis connection

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,24 +2,8 @@
 
 require 'redis'
 
-# Helper method to connect to Redis instance
-def connect_redis(env_var, default_url, name)
-  redis_url = ENV[env_var] || default_url
-  begin
-    Redis.new(url: redis_url)
-  rescue => e
-    Rails.logger.error("Failed to connect to #{name}: #{e.message}")
-    raise e
-  end
-end
+# Redis for primary usage
+$redis = Redis.new(url: ENV.fetch("REDIS_URL", "redis://redis:6379/0"))
 
-# Check if the application is precompiling assets
-if ENV['RAILS_ENV'] == 'assets'
-  puts "Skipping Redis connection during asset precompilation."
-else
-  # Initialize Redis connections
-  $redis = connect_redis('REDIS_URL', 'redis://localhost:6379/0', 'Redis')
-  $redis2 = connect_redis('REDIS2_URL', 'redis://localhost:6380/0', 'Redis2')
-
-  puts "Connected to Redis at #{ENV['REDIS_URL']} and Redis2 at #{ENV['REDIS2_URL']}"
-end
+# Redis2 for secondary usage
+$redis2 = Redis.new(url: ENV.fetch("REDIS2_URL", "redis://redis2:6380/0"))
