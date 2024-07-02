5668e77c23017b070c2f0c799a4d659a07655c83
gatha-krishnavamshi <krishnavamshi@gatha.co.in>
2024-07-02 13:39:23 +0530

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,34 @@
-Sidekiq.configure_server do |config|
-  sidekiq_config = YAML.load_file(Rails.root.join('config', 'sidekiq.yml'))
+# config/initializers/sidekiq.rb
 
-  config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
+require 'yaml'
+
+Sidekiq.configure_server do |config|
+  begin
+    sidekiq_config = YAML.load_file(Rails.root.join('config', 'sidekiq.yml'))
+  rescue Errno::ENOENT, Psych::SyntaxError => e
+    Rails.logger.error "Failed to load Sidekiq configuration: #{e.message}"
+    sidekiq_config = {}  # Provide a default configuration or handle as needed
+  end
+
+  config.redis = {
+    url: ENV['REDIS_URL'] || 'redis://localhost:6379/0',
+    network_timeout: 5
+  }
 
   if Rails.env.production?
-    config[:options] ||= {}
-    config[:options].merge!(sidekiq_config['production'] || {})
+    config.merge!(sidekiq_config['production'] || {})
   elsif Rails.env.staging?
-    config[:options] ||= {}
-    config[:options].merge!(sidekiq_config['staging'] || {})
+    config.merge!(sidekiq_config['staging'] || {})
   else
-    config[:options] ||= {}
-    config[:options].merge!(sidekiq_config['development'] || {})
+    config.merge!(sidekiq_config['development'] || {})
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', network_timeout: 5 }
+  config.redis = {
+    url: ENV['REDIS_URL'] || 'redis://localhost:6379/0',
+    network_timeout: 5
+  }
 end
+
+Sidekiq.logger = Rails.logger
