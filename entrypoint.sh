7b945d39cbc9c9b46e9dd9ebcf75462194fd41a8
gatha-krishnavamshi <krishnavamshi@gatha.co.in>
2024-07-19 16:15:11 +0530

    en

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-# Run database migrations
-bundle exec rake db:migrate
-
+# # Run database migrations
+# bundle exec rake db:migrate
+# Prepare the database (create, migrate, and seed if necessary)
+bundle exec rake db:prepare
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
 
